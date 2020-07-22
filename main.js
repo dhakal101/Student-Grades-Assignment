ed0dcbc88c309017a8fb27efce7619d3f64aec78
dhakal101 <50218546+gdhakal123@users.noreply.github.com>
2020-07-21 18:36:08 -0600

    s

--- main.js
+++ main.js
@@ -7,8 +7,11 @@ cnv.width = 800;
 cnv.height = 600;
 
 // Global Variables
-let grades = [60, 70, 45, 20, 40, 90, 100, 30, 45, 75, 40, 80];
-let max = 100; // grade values should be b/t 0 and max
+let grades = [];
+    for (let n = 0; n < 50; n++) {
+        grades.push(Math.randomInt(0, 100));
+    }
+    let max = 100; // grade values should be b/t 0 and max
 
 // Main Program Loop
 requestAnimationFrame(draw);
